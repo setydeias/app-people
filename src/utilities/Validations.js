9542691c2623570933bbb08541083d24adbd34f5
Neto Marçal <neto.marcal.dev@gmail.com>
2023-01-12 16:49:59 -0300

    Implementação da rotina clearForm

--- src/utilities/Validations.js
+++ src/utilities/Validations.js
@@ -41,3 +41,10 @@ export function isValidEMail (mail) {
     const regex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
     return regex.test(mail)
 }
+
+export function clearForm () {
+    var arrForms = document.forms
+      for (var j=0,f;f = arrForms[j];j++) {
+        f.reset()
+    }
+}
