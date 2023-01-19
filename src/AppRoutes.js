9fe46e167aff6a03ce5324df53297b21e95d0624
Neto Marçal <neto.marcal.dev@gmail.com>
2023-01-19 15:50:18 -0300

    Implementações relacionadas ao  Menu

--- src/AppRoutes.js
+++ src/AppRoutes.js
@@ -1,13 +1,19 @@
-import React from "react";
+import React, { useContext } from "react";
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import PrivateRoute from './auth';
 import Login from './pages/Login';
 import Dashboard from "./pages/Dashboard";
+import PrivateRoute from './auth';
+import { AuthContext } from './provider/authProviderMenu';
+import Menu from './components/Menu';
+
 
 const AppRoutes = () => {
 
+  const menuShow = useContext(AuthContext);
+
   return (
     <Router>
+         {menuShow.menuShow ? <Menu /> : ''}
       <Routes>
         <Route exact path="/" element={ <Login /> } />
         <Route exact path="/dashboard" element={ 
--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import { AuthProviderMenu } from './provider/authProviderMenu';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  <AuthProviderMenu>
+    <React.StrictMode>
       <App />
-  </React.StrictMode>
+    </React.StrictMode>
+  </AuthProviderMenu>
 );
 
