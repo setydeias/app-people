d03360a127760936a9e3d362ef748097ff49feba
Neto Marçal <neto.marcal.dev@gmail.com>
2023-01-30 17:14:17 -0300

    Implementações teste relacionadas a validação de senha

--- src/pages/ChangePassword/index.js
+++ src/pages/ChangePassword/index.js
@@ -1,6 +1,6 @@
 import React, { useState, useRef } from 'react';
 import './styles.css';
-import { Form, FormGroup, Button, Alert } from 'reactstrap';
+import { Form, FormGroup, Button, Alert, Container } from 'reactstrap';
 import Img from "../../assets/imgs/Login01.jpg";
 import { login } from '../../api/User';
 import Spinner from '../../components/Spinner';
@@ -15,6 +15,7 @@ const Login = (props) => {
   var referencces = {
     userPassword: useRef(null),
     userPasswordConfirm: useRef(null),
+    userForcePassword: useRef(null)
   }
 
   const validateStatus = {
@@ -50,9 +51,6 @@ const Login = (props) => {
     const { name, value } = e.target;
     setUser({ ...user, [name]: value });
     setMessage('');
-    console.log(user.user_password_new);
-    console.log(user.user_password_confirm);
-
   };
 
   const handleSubmit = async (e) => {
@@ -60,8 +58,13 @@ const Login = (props) => {
     setMessage('');
     try { 
 
+      console.log(`Senha: ${user.user_password_new} Confirme: ${user.user_password_confirm}`);
 
-       alert(testPassword());
+      if(validPassword()){
+        alert('Senha validada!');
+        return;
+      }
+      alert('Erro ao validar senha!');
 
       /*
          const resp = await login({ document: user.user_document, password_user: user.user_password }); 
@@ -96,10 +99,57 @@ const Login = (props) => {
     e.preventDefault();
     setUser({ ...user, showPasswordConfirm : !user.showPasswordConfirm });
   }
+
+  const testForcePassword = () => {
+
+    var force = 0;
+	  
+	  if((user.user_password_new.length >= 4) && (user.user_password_new <= 7)){
+	  	force += 10;
+	  }else if(user.user_password_new.length > 7){
+	  	force += 25;
+	  }
+
+	  if((user.user_password_new.length >= 5) && (user.user_password_new.match(/[a-z]+/))){
+	  	force += 10;
+	  }
+
+	  if((user.user_password_new.length >= 6) && (user.user_password_new.match(/[A-Z]+/))){
+	  	force += 20;
+	  }
+
+	  if((user.user_password_new.length >= 7) && (user.user_password_new.match(/[@#$%&;*]/))){
+	  	force += 25;
+	  }
+
+	  if(user.user_password_new.match(/([1-9]+)\1{1,}/)){
+	  	force += -25;
+	  }
+
+    showForcePassword(force);
+  }
+
+  const showForcePassword = (force) => {
+
+    referencces.userForcePassword.current.innerHTML = "";
+    
+    if(force < 30 ){
+      referencces.userForcePassword.current.innerHTML = "<span style='color: #790505'>Senha Fraca!</span>";
+    }else if((force >= 30) && (force < 50)){
+      referencces.userForcePassword.current.innerHTML = "<span style='color: #fab027'>Senha Média</span>";
+    }else if((force >= 50) && (force < 70)){
+      referencces.userForcePassword.current.innerHTML = "<span style='color: #105510'>Senha Forte</span>";
+    }else if((force >= 70) && (force < 100)){
+      referencces.userForcePassword.current.innerHTML = "<span style='color: #0c270c'>Senha Excelente</span>";
+    }
+  }
  
   const testPassword = () => {
-    if (user.user_password_new === '') {
-      referencces.userPassword.current.focus();
+
+    testForcePassword();
+    
+    if (testLengthPassword() && user.user_password_new === '') {
+      referencces.userPassword.current.focus(); 
       setUser({
         ...user,
         status: {
@@ -125,6 +175,76 @@ const Login = (props) => {
     return true;
   }
 
+  const testPasswordConfirm = () => {
+    if(user.user_password_confirm != user.user_password_new){
+      setUser({
+        ...user,
+        status: {
+          passwordConfirm: {
+            erro: 'Senhas divergentes!',
+            validate: validateStatus.invalide
+          },
+          password:  {
+            erro: '',
+            validate: validateStatus.invalide
+          }
+        }
+      });
+      return false;
+    }
+    setUser({
+      ...user,
+      status: {
+        passwordConfirm: {
+          erro: '',
+          validate: validateStatus.valide
+        },
+        password: {
+          erro: '',
+          validate: validateStatus.valide
+        }
+      }
+    });
+    return true; 
+  }
+
+  const testLengthPassword = () => {
+    if (user.user_password_new.length < 6) {
+      referencces.userForcePassword.current.innerHTML = "";
+      setUser({
+        ...user,
+        status: {
+          password: {
+            erro: 'A Senhas não pode ser menor que 6 caracteres.',
+            validate: validateStatus.invalide
+          },
+          passwordConfirm: formStatusDefault
+        }
+      });
+      return false;
+    }
+    setUser({
+      ...user,
+      status: {
+        password: {
+          erro: '',
+          validate: validateStatus.valide
+        },
+        passwordConfirm: formStatusDefault
+      }
+    });
+    return true;
+  }
+  
+  const validPassword = () => {
+    if(testLengthPassword()){
+      if (testPassword() && testPasswordConfirm()) {
+        return true
+      }
+    }    
+    return false;
+  }
+
   return(
     <div className="body">
       <div className="left-login">
@@ -135,7 +255,7 @@ const Login = (props) => {
         <div className="card-login">
           <Form className="login-form" onSubmit={ handleSubmit }>
             <h3>Cadastrar senha</h3>
-            <p className='caption'>Crie uma senha de acesso a sua conta Setydeias</p>
+            <p className='caption'>Crie uma senha de acesso a sua conta Setydeias contendo no mínimo 6 caracteres.</p>
             <FormGroup>
               <div className="input-group has-validation">
                 <span className="input-group-text"><i className="fas fa-id-card"></i></span>
@@ -147,8 +267,8 @@ const Login = (props) => {
                     name='user_password_new' 
                     ref={ referencces.userPassword }
                     onChange={ handleChange }
-                    onBlur={ testPassword }
-                    placeholder="UserDocument" 
+                    onBlur={ validPassword }
+                    placeholder="" 
                     required 
                   />
                   <label for="user-document">Senha</label>
@@ -163,6 +283,7 @@ const Login = (props) => {
                 <div className="invalid-feedback">
                  { user.status.password.erro }
                 </div>
+                <div ref={referencces.userForcePassword}></div>
               </div>
               <div className="input-group has-validation">
                 <span className="input-group-text"><i className="fas fa-id-card"></i></span>
@@ -172,10 +293,10 @@ const Login = (props) => {
                     className={user.status.passwordConfirm.validate} 
                     id="user_password_confirm" 
                     name='user_password_confirm' 
-                    ref={ referencces.userPassword }
+                    ref={ referencces.userPasswordConfirm }
                     onChange={ handleChange }
-                    onBlur={ ()=>{} }
-                    placeholder="UserDocument" 
+                    onBlur={ validPassword }
+                    placeholder="" 
                     required 
                   />
                   <label for="user-document">Confirmar</label>
@@ -188,7 +309,7 @@ const Login = (props) => {
                   <i className="fas fa-eye"></i>
                 </span>
                 <div className="invalid-feedback">
-                 { user.status.password.erro }
+                 { user.status.passwordConfirm.erro }
                 </div>
               </div>
             </FormGroup>                
