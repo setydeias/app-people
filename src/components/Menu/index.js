91dada38a12caff35bc117b66ea269bbe324c7c4
Neto Marçal <neto.marcal.dev@gmail.com>
2023-02-13 12:16:13 -0300

    Adapatando o menu para a necessidade atual

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -2,7 +2,6 @@ import React, { useEffect, useState, useRef }  from 'react';
 import { Link } from 'react-router-dom';
 import './styles.css';
 import { getTopNav } from '../../data/navbars';
-import { logImg } from '../img/logo.png';
 
 const Menu = () => { 
 
@@ -27,39 +26,25 @@ const Menu = () => {
 
 
   return(
-      <>       
-      <div className="nav__wrapper">
-          <div className="container">
-            <nav className="nav">
-              <Link to="#" className="nav__brand" style={{ color: '#fff', textDecoration: 'none'}}>
-                {/*<img src="http://setydeias.com.br/wp-content/uploads/2020/02/Nome-da-Logo-1-1.png" width="200" height="40" alt="" loading="lazy" />*/}
-                <img src={logImg} width="50" height="50" alt="" loading="lazy" /> Setydeias cadastro
-              </Link>
-              <ul className={collapse}>
-                <li className="nav__item">
-                  <Link to={"/admin"} className="nav__link" style={{ color: '#fff', textDecoration: 'none'}}><i className="fas fa-home fa-1x"></i> Início <span className="sr-only">(current)</span></Link>
-                </li>
-                <li className="nav__item dropdown">
-                  <li className="nav__link dropdown-toggle" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false" style={{ color: '#fff', textDecoration: 'none'}}><i className="fas fa-users fa-1x"></i> Clientes <span className="sr-only">(current)</span></li>
-                  <div className="dropdown-menu" aria-labelledby="navbarDropdown">
-                    <Link className="dropdown-item" to={""}><i className="fas fa-user-edit"></i> Alterar</Link>
-                    <Link className="dropdown-item" to={""}><i className="fas fa-search"></i> Consultar</Link>
-                    <Link className="dropdown-item" to={""}><i className="fas fa-user-minus"></i> Excluir</Link>
-                  </div>
-                </li>
-                <li className="nav__item">
-                  <Link to={"/logout"} className="nav__link"  id="link_logout" style={{ color: '#fff', textDecoration: 'none'}}><i className="fas fa-sign-out-alt fa-1x"></i> Sair<span className="sr-only">(current)</span></Link>
-                </li>
-              </ul>
-              <div className={toggleIcon} onClick={onToggle}>
-                <div className="line__1"></div>
-                <div className="line__2"></div>
-                <div className="line__3"></div>
-              </div>
-            </nav>
+    <div className="nav__wrapper shadow p-3 mb-5 bg-body rounded">
+      <div className="container">
+        <nav className="nav">
+          <Link to="#" className="nav__brand" style={{ color: '#fff', textDecoration: 'none'}}>
+            <img src="http://setydeias.com.br/wp-content/uploads/2020/02/Nome-da-Logo-1-1.png" width="200" height="40" alt="Logo Setydeias" loading="lazy" />
+          </Link>
+          <ul className={collapse}>             
+            <li className="nav__item">
+              <Link to={"/logout"} className="nav__link"  id="link_logout" style={{ color: '#000', textDecoration: 'none'}}><i className="fas fa-sign-out-alt fa-1x"></i> Sair<span className="sr-only">(current)</span></Link>
+            </li>
+          </ul>
+          <div className={toggleIcon} onClick={onToggle}>
+            <div className="line__1"></div>
+            <div className="line__2"></div>
+            <div className="line__3"></div>
           </div>
+        </nav>
       </div>
-      </>
+    </div>
   )
 
 }
--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -2,9 +2,20 @@ import React from "react";
 
 const Dashboard = () => {
 
-    return(
-        <>Dashboard</>
-    );
+  return(
+    <div className='container-sm'>
+      <h4 className='title-page'>Cadastro único</h4>
+      <nav>
+        <div className="nav nav-tabs d-flex justify-content-start" id="nav-tab" role="tablist">
+          <button className="nav-link active" id="nav-home-tab" data-bs-toggle="tab" data-bs-target="#nav-home" type="button" role="tab" aria-controls="nav-home" aria-selected="true"><i class="fas fa-user"></i> Dados Pessoal</button>
+          <button className="nav-link" id="nav-api-tab" data-bs-toggle="tab" data-bs-target="#nav-api" type="button" role="tab" aria-controls="nav-api" aria-selected="false"><i class="fas fa-id-card"></i> Nível da conta</button>
+        </div>
+     </nav>
+     <form className='row' onSubmit={ ()=>{} }> 
+
+     </form>
+    </div>
+  );
 
 }
 
